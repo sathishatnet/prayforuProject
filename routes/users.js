fee6ea4be46bf17476d1e4981dfb499c46a8e31e
sathishatnet <sathishatnet@gmail.com>
2015-02-16 17:00:20 +0530

    Test Page - Part 2

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('index', { title: 'Users' });
+  res.render('index', { title: 'Users' });
 });
 
 module.exports = router;
